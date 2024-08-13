ad84422e0627ff435d6f48b414d83cfd2b0716a6
Pedro Otávio <pedrootavio09@yahoo.com.br>
2024-08-13 15:05:58 -0300

    localStorage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 import ehUmCPF from "./valida-cpf.js";
 import ehMaiorDeIdade from "./valida-idade.js";
 const camposDoFormulario = document.querySelectorAll("[required]");
+const formulario = document.querySelector("[data-formulario]");
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const listaRespostas = {
+        "nome": e.target.elements["nome"].value,
+        "email": e.target.elements["email"].value,
+        "rg": e.target.elements["rg"].value,
+        "cpf": e.target.elements["cpf"].value,
+        "aniversario": e.target.elements["aniversario"].value,
+    }
+    localStorage.setItem("cadastro", JSON.stringify(listaRespostas));
+
+    window.location.href = "./abrir-conta-form-2.html";
+})
 
 camposDoFormulario.forEach((campo) => {
     campo.addEventListener("blur", () => verificaCampo(campo));
@@ -56,7 +72,7 @@ function verificaCampo(campo) {
         ehMaiorDeIdade(campo);
     }
     tiposDeErro.forEach(erro => {
-        if(campo.validity[erro]){
+        if(campo.validity[erro]) {
             mensagem = mensagens[campo.name][erro];
         }
     })
